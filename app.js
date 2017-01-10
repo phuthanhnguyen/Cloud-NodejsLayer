ee8acf6be4a527fcf7e3e5be2870b636da45ef59
phuthanhnguyen <nguyenphuthanh@NGUYENs-MacBook-Pro.local>
2017-01-10 16:09:29 +0100

    first commit

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+var login = require('./routes/login');
+var signup = require('./routes/signup');
 
 var app = express();
 
@@ -21,10 +22,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
 
-app.use('/', index);
-app.use('/users', users);
-
+app.post('/login', login.login);
+app.post('/signup',signup.signup);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
@@ -42,5 +43,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
--- routes/signup.js
+++ routes/signup.js
@@ -1,16 +1,15 @@
 "use strict";
 var request = require('superagent');
 
-exports.login = function(req, res) {
+exports.signup = function(req, res) {
 	var json = JSON.stringify(req.body);
 	console.log(json);
 	request
-	   .post('http://10.32.0.206:8000/authentication/login/')
+	   .post('http://10.32.0.206:8000/authentication/signUp/')
 	   .set('Accept', 'application/json')
 	   .send(json)
 	   .end(function(err, response){
-	   		console.log(response.body);
-	   		//res.send(200);
-	     	//res.json(response.body);
+	   		console.log(response);
+	     	res.json(response.body);
 	   });
 };
